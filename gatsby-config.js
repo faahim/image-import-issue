4751cdf11a9c3be1e9ebde861bd4c1ff72a88308
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-09-04 12:50:32 +0600

    Updated path of markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/_posts/blog`,
+        path: `${__dirname}/`,
         name: 'markdown-pages',
       },
     },
